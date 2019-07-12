4ec41aa977dd15993104f0735f3e301e0eb4f4d6
[maciej] <[md9u17@soton.ac.uk]>
2019-07-13 00:03:06 +0100

    chapter error handling

--- parsing.c
+++ parsing.c
@@ -20,40 +20,87 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #include <editline/history.h>
 #endif
-
+/*Declare New lval struct*/
+typedef struct {
+  int type;
+  long num;
+  int err;
+}lval;
+/*Create Enumeration of Possible lval Types*/
+enum { LVAL_NUM, LVAL_ERR };
+/*Create Enumeration of Possible Errors*/
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+/*Create a new number type lval*/
+lval lval_num(long x){
+  lval v;
+  v.type = LVAL_NUM;
+  v.num = x;
+  return v;
+}
+/*Create a new error type lval*/
+lval lval_err(int x){
+  lval v;
+  v.type = LVAL_ERR;
+  v.err = x;
+  return v;
+}
+/*Print an "lval"*/
+void lval_print(lval v){
+  switch (v.type) {
+    case LVAL_NUM: printf("%li", v.num); break;
+    case LVAL_ERR:
+      if(v.err==LERR_DIV_ZERO){ printf("Error: Invalid Operator!"); }
+      if(v.err==LERR_BAD_OP){ printf("Error: Invalid Operator!"); }
+      if(v.err==LERR_BAD_NUM){ printf("Error: Invalid Number!");}
+      break;
+  }
+}
+/*Print lval followed by new line*/
+void lval_println(lval v){ lval_print(v); putchar('\n'); }
 /* Use operator string to see which operation to perform */
-long eval_op(long x, char* op, long y) {
-  if (strcmp(op, "+") == 0) { return x + y; }
-  if (strcmp(op, "-") == 0) { return x - y; }
-  if (strcmp(op, "*") == 0) { return x * y; }
-  if (strcmp(op, "/") == 0) { return x / y; }
-  if (strcmp(op, "%") == 0) { return x % y; }
-  if (strcmp(op, "^") == 0) { return pow(x,y); }
-  if (strcmp(op, "min") == 0) { return ((x<y)?x:y); }
-  if (strcmp(op, "max") == 0) { return ((x>y)?y:x); }
-  return 0;
+lval eval_op(lval x, char* op, lval y) {
+  /*If either value is an error return it*/
+  if(x.type==LVAL_ERR){ return x; }
+  if(y.type==LVAL_ERR){ return y; }
+  /*Otherwise do maths*/
+  if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+  if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+  if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+  if (strcmp(op, "/") == 0) {
+    /*If the second operand is zero return error*/
+    return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num); 
+    }
+  if (strcmp(op, "%") == 0) {
+    /*Same as before*/
+    return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num % y.num);
+  }
+  if (strcmp(op, "^") == 0) {
+    return (x.num == 0 && y.num == 0) ? lval_err(LERR_BAD_NUM) : lval_num(pow(x.num,y.num)); 
+    }
+  if (strcmp(op, "min") == 0) { return lval_num((x.num<y.num)?x.num:y.num); }
+  if (strcmp(op, "max") == 0) { return lval_num((x.num>y.num)?x.num:y.num); }
+  
+  return lval_err(LERR_BAD_OP);
 }
 
-long eval(mpc_ast_t* t) {
+lval eval(mpc_ast_t* t) {
   
-  /* If tagged as number return it directly. */ 
+  /*Check if there is some error in conversion*/ 
   if (strstr(t->tag, "number")) {
-    return atoi(t->contents);
+    errno = 0;
+    long x = strtol(t->contents, NULL, 10);
+    return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);  //errno seems to be error indicator from the eerno.h library
   }
-  
   /* The operator is always second child. */
   char* op = t->children[1]->contents;
-  
   /* We store the third child in `x` */
-  long x = eval(t->children[2]);
-  
+  lval x = eval(t->children[2]);
   /* Iterate the remaining children and combining. */
   int i = 3;
   while (strstr(t->children[i]->tag, "expr")) {
     x = eval_op(x, op, eval(t->children[i]));
     i++;
   }
-  
   return x;  
 }
 
@@ -99,8 +146,9 @@ int main(int argc, char** argv) {
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
       /* On success print and delete the AST */
-      mpc_ast_print(r.output);
-      mpc_ast_delete(r.output);
+      lval result = eval(r.output);   //output is mpc_val_t, declared typedef void mpc_val_t; why void stores all the data?
+      lval_println(result);           //it actually have a sense, mpc_val_t is a alias to void; and it creates a pointer void*
+      mpc_ast_delete(r.output);       //void pointers have a meaning - they just dont specify type of data they are pointing to - no void variables!
     } else {
       /* Otherwise print and delete the Error */
       mpc_err_print(r.error);
